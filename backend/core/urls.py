2a0188ed5e2dfe4a85a2b00d67332bcd621e49ed
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-11-17 13:21:31 +0530

    removed unwanted folders

--- backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-# core/urls.py
-from django.urls import path
-from .views import ModelDownloadView
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('download-encrypted-model/', ModelDownloadView.as_view(), name='download-encrypted-model'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
